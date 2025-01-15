47883ec07da628c7dee51c459ed5606ad38c50f5
tipusahil <tipusahil.ctg@gmailcom>
2025-01-15 22:07:02 +0600

    2nd commit

---
+++ loop.js
@@ -0,0 +1,14 @@
+
+var fruits = ['am',  'katal','banana', 'lisu', 'dragonfol'];
+for(let x=1; x< 5; x++){
+    console.log(fruits[x]);
+}
+
+
+//.................while loop.........
+var number  = [43, 3, 22, 45,  65, 83, 32];
+var y= 0;
+while (y<5){
+console.log(number[y])
+y=y+1;
+}
