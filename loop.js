26682be0a15521d2986a3f667b87d148053c7a9e
tipusahil <tipusahil.ctg@gmailcom>
2025-01-15 22:55:35 +0600

    3rd commit

--- loop.js
+++ loop.js
@@ -6,9 +6,19 @@ for(let x=1; x< 5; x++){
 
 
 //.................while loop.........
-var number  = [43, 3, 22, 45,  65, 83, 32];
+// var 
+number  = [43, 3, 22, 45,  65, 83, 32];
 var y= 0;
 while (y<5){
-console.log(number[y])
+console.log(number[y]);
 y=y+1;
 }
+
+//......do while loop.............
+
+var num = [43,55,6,32,2];
+var z = 0;
+do{
+    console.log(num[z]);
+z++;
+}while(z<5);
